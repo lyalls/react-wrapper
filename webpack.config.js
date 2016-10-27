267af7b41f9253217ca7512cf86bf8067dd57b45
Lin Sun <lyall.sunlin@gmail.com>
2016-10-27 19:44:17 +0800

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
     },
     entry: {
         index: __dirname + "/src/apps/mobile/index.js",
-        common: ['react', 'redux', 'react-redux', 'material-ui', 'react-dom', 'jquery' ,'redux-thunk', 'redux-logger']
+        common: ['react', 'redux', 'react-redux', 'material-ui', 'react-dom', 'jquery' ,'redux-thunk', 'redux-logger'],
+        css: __dirname + "/src/css/app.less"
     },
     output: {
         path: __dirname + "/built",
@@ -26,6 +27,22 @@ module.exports = {
                     presets: ['react', 'es2015', 'es2016-node5', 'stage-0'],
                     plugins: ['transform-runtime']
                 }
+            },
+            {
+                test: /\.less$/,
+                loaders: ['style', 'css', 'less']
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css']
+            }, 
+            {
+                test: /\.json$/,
+                loader: 'json'
+            },
+            {
+                test: /\.(jpe?g|gif|png|eot|svg|woff|woff2|ttf)$/,
+                loaders: ['file']
             }
         ]
     }
