066a547ac54a107d283f4c0d662c90cce72b2462
Lin Sun <mr.lin.sun@gmail.com>
2016-12-27 10:35:49 +0800

    Fix git problem of untracked files

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -1,4 +1,5 @@
 import React, {Component, PropTypes} from 'react';
+import { Modal,ModalManager,Effect} from 'react-dynamic-modal';
 
 class Dialog extends Component {
 	constructor(props) {
@@ -6,27 +7,56 @@ class Dialog extends Component {
 		
 	}
 	dismiss(){
-
-		if(this.props.dismissed) this.props.dismissed();
+		if(this.props.dismiss) this.props.dismiss();
+		ModalManager.close();
 	}
+
+	// Dialog
+    afterOpenModal() {
+    	console.log('Dialog modal is open');
+    }
+
+
+
 	render(){
 		return(
-			<div className="k_layer">
-		        <div className="k_layer_txt"><p>{this.props.content}</p></div>
-		        <div className="common_popup_bottom_btns">
-		            <button onClick={this.dismiss} className="single_btn">
-		            	{(this.props.dismissTitle === undefined)?"确定":this.props.dismissTitle}
-		            </button>
-		        </div>
-		    </div>
+			<Modal 
+                effect={Effect.SuperScaled}
+                onRequestClose={this.dismiss.bind(this)}
+                onAfterOpen={this.afterOpenModal}
+                contentLabel="Dialog Modal"
+                style={{content : {
+                    
+                }}}
+            >
+                <div className="k_layer">
+                    <div className="k_layer_txt"><p>{this.props.content}</p></div>
+                    <div className="common_popup_bottom_btns">
+                        <button onClick={this.dismiss.bind(this)} className="single_btn">
+                        {  
+                        	this.props.dismissTitle || '确定'
+                        }
+                        </button>
+                    </div>
+                </div>
+            </Modal>
 		)
 	}
 }
 
 Dialog.propTypes = {
-	content: PropTypes.string.isRequired,
-	dismissed: PropTypes.func,
+	content: PropTypes.string,
+	dismiss: PropTypes.func,
 	dismissTitle: PropTypes.string,
 }
 
 export default Dialog;
+export function OpenDialog({content, dismiss, dismissTitle}){
+
+	let dialog = <Dialog 
+        content = {content || ""}
+        dismiss = {dismiss}
+        dismissTitle = {dismissTitle}
+    /> ;
+	ModalManager.open(dialog);
+}
